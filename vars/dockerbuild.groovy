e39e633bfb150354b7b34a7145a8e9ced4edb75c
Deekshith SN <deekshithsn@gmail.com>
2021-01-06 19:42:55 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,10 +1,7 @@
 def call(String project) {
 
 
-  def getDockerTag(){
-        def tag = sh script: 'git rev-parse HEAD', returnStdout: true
-        return tag
-        }
+
         
   sh 'docker build . -t deekshithsn/devops-training:$Docker_tag'
 		   withCredentials([string(credentialsId: 'docker_password', variable: 'docker_password')]) {
